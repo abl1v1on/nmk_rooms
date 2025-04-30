0d89d7d2d336fe69f3e11aad3bdd0bda2ec4510e
Maxim Danilov <abl1v1on@yandex.ru>
2025-04-30 15:28:04 +0300

    add auth api service

---
+++ backend/src/api/auth/service.py
@@ -0,0 +1,26 @@
+import jwt
+
+from core import settings
+
+
+class AuthAPIService:
+    def __init__(self) -> None:
+        self.algorithm = settings.auth_jwt.algorithm
+        self.private_key = settings.auth_jwt.private_key_path.read_text()
+        self.public_key = settings.auth_jwt.public_key_path.read_text()
+
+    def encode_jwt(self, payload: dict) -> str:
+        token = jwt.encode(
+            payload=payload,
+            key=self.private_key,
+            algorith=self.algorithm
+        )
+        return token
+
+    def decode_jwt(self, token: str | bytes) -> dict:
+        data = jwt.decode(
+            jwt=token,
+            key=self.public_key,
+            algorithms=[self.algorithm]
+        )
+        return data
