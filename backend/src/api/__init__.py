633dd5e54736560515f3e0a7e8635d3c1e97d7fa
Maxim Danilov <abl1v1on@yandex.ru>
2025-04-30 16:09:18 +0300

    include auth router to main router

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .users import users_router
+from .auth import auth_router
 
 
 main_api_router = APIRouter(prefix='/api/v1')
 main_api_router.include_router(users_router)
+main_api_router.include_router(auth_router)
