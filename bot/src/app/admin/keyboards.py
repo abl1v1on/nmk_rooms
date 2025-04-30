eab4ee13b0549b331b1e4b43cfb107974401089d
Maxim Danilov <abl1v1on@yandex.ru>
2025-04-30 13:16:03 +0300

    set one time keyboard = True

--- bot/src/app/admin/keyboards.py
+++ bot/src/app/admin/keyboards.py
@@ -4,5 +4,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 admin_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='👤 Добавить пользователя 👤')]
-    ], resize_keyboard=True
+    ], resize_keyboard=True, one_time_keyboard=True
 )
