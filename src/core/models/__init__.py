9360de7bcd38dcf55f06beeac708a872d66151ac
Maxim Danilov <abl1v1on@yandex.ru>
2025-04-30 11:23:02 +0300

    add users sqla model

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,7 +2,9 @@ __all__ = [
     'Base',
     'SESSION_DEP',
     'db_helper',
+    'User',
 ]
 
 from .base import Base
 from .db_helper import SESSION_DEP, db_helper
+from .users import User
---
+++ src/core/models/users.py
@@ -0,0 +1,30 @@
+from sqlalchemy import String, CheckConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from . import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+    first_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    tg_id: Mapped[int] = mapped_column(nullable=False)
+
+    __table_args__ = (
+        CheckConstraint(
+            'LENGTH(first_name) >= 2',
+            name='check_user_first_name_length'
+        ),
+        CheckConstraint(
+            'LENGTH(last_name) >= 2',
+            name='check_user_last_name_length'
+        ),
+    )
