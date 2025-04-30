68f8f666795bc9d1a71a958ed971bffa4fdac2a3
Maxim Danilov <abl1v1on@yandex.ru>
2025-04-30 19:23:45 +0300

    include rooms router to main router

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .users import users_router
 from .auth import auth_router
+from .rooms import rooms_router
 
 
 main_api_router = APIRouter(prefix='/api/v1')
 main_api_router.include_router(users_router)
 main_api_router.include_router(auth_router)
+main_api_router.include_router(rooms_router)
--- backend/src/core/models/__init__.py
+++ backend/src/core/models/__init__.py
@@ -3,8 +3,12 @@ __all__ = [
     'SESSION_DEP',
     'db_helper',
     'User',
+    'Room',
+    'Location',
 ]
 
 from .base import Base
 from .db_helper import SESSION_DEP, db_helper
 from .users import User
+from .rooms import Room
+from .locations import Location
