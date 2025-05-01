0b6121b7bd5bb07d31e862882856f41cc09f07f0
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-01 12:26:52 +0300

    add loader component

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+    return (
+    <div className="has-text-centered" style={{ padding: '2rem' }}>
+      <button className="button is-loading is-white is-large" style={{ border: 'none' }}>
+        Загрузка...
+      </button>
+    </div>
+  );
+}
