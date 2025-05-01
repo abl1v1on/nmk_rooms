d398dc219c68d1fd7ddc0292093c4a9b20af1427
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-01 09:38:46 +0300

    include locations router to main router

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .users import users_router
 from .auth import auth_router
 from .rooms import rooms_router
+from .locations import locations_router
 
 
 main_api_router = APIRouter(prefix='/api/v1')
 main_api_router.include_router(users_router)
 main_api_router.include_router(auth_router)
 main_api_router.include_router(rooms_router)
+main_api_router.include_router(locations_router)
---
+++ backend/src/api/locations/__init__.py
@@ -0,0 +1,1 @@
+from .routes import router as locations_router
