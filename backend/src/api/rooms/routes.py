129bc26f7186909e25852acf6e43cc5f3287eae8
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-01 10:44:28 +0300

    add routes

--- backend/src/api/rooms/routes.py
+++ backend/src/api/rooms/routes.py
@@ -1,4 +1,23 @@
 from fastapi import APIRouter
+from pydantic import PositiveInt
+
+from .service import SERVICE_DEP
+from .schemas import GetRoomSchema, CreateRoomSchema
 
 
 router = APIRouter(prefix='/rooms', tags=['Залы'])
+
+
+@router.get('/', response_model=list[GetRoomSchema])
+async def get_rooms(service: SERVICE_DEP):
+    return await service.get_rooms()
+
+
+@router.get('/{room_id}', response_model=GetRoomSchema | None)
+async def get_room(service: SERVICE_DEP, room_id: PositiveInt):
+    return await service.get_room(id=room_id)
+
+
+@router.post('/', response_model=GetRoomSchema)
+async def create_room(service: SERVICE_DEP, room: CreateRoomSchema):
+    return await service.create_room(room)
