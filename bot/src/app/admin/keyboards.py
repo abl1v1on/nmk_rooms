910e091753475254b842a3c114aec8fa5f25f3fd
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-01 09:52:03 +0300

    add get locations btn

--- bot/src/app/admin/keyboards.py
+++ bot/src/app/admin/keyboards.py
@@ -3,7 +3,13 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 admin_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text='👤 Добавить пользователя 👤')],
-        [KeyboardButton(text='👥 Список пользователей 👥')],
+        [
+            KeyboardButton(text='👤 Добавить пользователя 👤'),
+            KeyboardButton(text='👥 Список пользователей 👥')
+        ],
+        [
+            KeyboardButton(text='🗺 Добавить локацию 🗺'),
+            KeyboardButton(text='🗺 Список локаций 🗺')
+        ],
     ], resize_keyboard=True, one_time_keyboard=True
 )
