ff12fd0f726d93c0c0412b5678381367db35156e
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-03 11:28:57 +0300

    add delete all rooms route

--- backend/src/api/rooms/routes.py
+++ backend/src/api/rooms/routes.py
@@ -21,3 +21,13 @@ async def get_room(service: SERVICE_DEP, room_id: PositiveInt):
 @router.post('/', response_model=GetRoomSchema)
 async def create_room(service: SERVICE_DEP, room: CreateRoomSchema):
     return await service.create_room(room)
+
+
+# @router.delete('/{room_id}', response_model=None)
+# async def delete_room(service: SERVICE_DEP, room_id: PositiveInt):
+#     return await service.delete_room(room_id)
+
+
+@router.delete('/delete-all', response_model=None)
+async def delete_all_rooms(service: SERVICE_DEP):
+    return await service.delete_all_rooms()
