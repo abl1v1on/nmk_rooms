1818ef0d5d02dd39273837fa69690d3e9f1220a2
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-04 10:33:46 +0300

    include bookings router to main router

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -5,6 +5,7 @@ from .auth import auth_router
 from .rooms import rooms_router
 from .locations import locations_router
 from .equipments import equipments_router
+from .bookings import bookings_router
 
 
 main_api_router = APIRouter(prefix='/api/v1')
@@ -13,3 +14,4 @@ main_api_router.include_router(auth_router)
 main_api_router.include_router(rooms_router)
 main_api_router.include_router(locations_router)
 main_api_router.include_router(equipments_router)
+main_api_router.include_router(bookings_router)
