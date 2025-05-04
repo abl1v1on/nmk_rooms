2ae28969fe57f49ab14386b4332144ffdd933216
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-04 11:24:54 +0300

    add exception

--- backend/src/api/bookings/exceptions.py
+++ backend/src/api/bookings/exceptions.py
@@ -7,3 +7,11 @@ class BookingNotFoundException(HTTPException):
             detail='Booking not found',
             status_code=status.HTTP_404_NOT_FOUND
         )
+
+
+class BookingAlreadyExistsException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            detail='Booking already exists',
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
