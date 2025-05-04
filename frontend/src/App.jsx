fdc86f6d7ee5963167d33aa5ae8eb510b2943ac8
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-04 12:49:28 +0300

    add route to my bookings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer.jsx";
 import MainPage from "./pages/MainPage.jsx";
 import HallListPage from "./pages/HallListPage.jsx";
 import HallDetailsPage from "./pages/HallDetailsPage.jsx";
+import MyBookingsPage from "./pages/MyBookingsPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 
@@ -21,6 +22,7 @@ export default function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="/halls" element={<HallListPage />} />
             <Route path="/halls/:id" element={<HallDetailsPage />} />
+            <Route path="/my-bookings" element={<MyBookingsPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
