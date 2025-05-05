eae72f506588bef3dd256026b89a343df4f557ee
Maxim Danilov <abl1v1on@yandex.ru>
2025-05-05 14:06:58 +0300

    add http bearer auth schema

--- backend/src/api/auth/routes.py
+++ backend/src/api/auth/routes.py
@@ -1,7 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from .service import SERVICE_DEP
-from .schemas import AuthUserSchema, AccessTokenSchema
+from .schemas import AuthUserSchema, AccessTokenSchema, TokenPayload
+from api.users.service import SERVICE_DEP as USERS_SERVICE_DEP
+
+
+http_bearer = HTTPBearer()
+
+
+def get_auth_token(
+        service: SERVICE_DEP,
+        jwt_credentials: HTTPAuthorizationCredentials = Depends(http_bearer)
+    ) -> TokenPayload:
+    token = jwt_credentials.credentials
+    payload = TokenPayload(**service.decode_jwt(token))
+    return payload
+
+
+async def get_current_user(
+        service: USERS_SERVICE_DEP,
+        payload: TokenPayload = Depends(get_auth_token)
+    ):
+    user = await service.get_user(id=int(payload.sub))
+    return user
 
 
 router = APIRouter(prefix='/auth', tags=['Аутентификация'])
